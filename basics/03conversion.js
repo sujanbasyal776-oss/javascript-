000d1951f662fcaa72449a52b3aa40e91707785c
sujanbasyal776-oss <sujanbasyal776@gmail.com>
2026-06-26 07:56:28 +0000

    sujan

---
+++ basics/03conversion.js
@@ -0,0 +1,13 @@
+let score="33abcc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+//33=> 33
+//33abc=> NaN
+//sujan=>NaN
+//true=>1
+//false=>0
